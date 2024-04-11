3a5fba65fe54bc1523ddfc1de2c5fdb2f509eb60
Changju Lee <rookiecj@gmail.com>
2024-04-11 17:18:15 +0900

    add store

---
+++ src/example/calc.rs
@@ -0,0 +1,95 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+use store_rs::{Store, Reducer, Subscriber};
+
+#[derive(Debug)]
+enum CalcAction {
+    Add(i32),
+    Subtract(i32),
+}
+
+
+struct CalcReducer {}
+
+
+impl Default for CalcReducer {
+    fn default() -> CalcReducer {
+        CalcReducer {}
+    }
+}
+
+
+#[derive(Debug, Clone)]
+struct CalcState {
+    count: i32,
+}
+
+impl Default for CalcState {
+    fn default() -> CalcState {
+        CalcState { count: 0 }
+    }
+}
+
+impl Reducer<CalcState, CalcAction> for CalcReducer {
+    fn reduce(&self, state: &CalcState, action: &CalcAction) -> CalcState {
+        match action {
+            CalcAction::Add(i) => {
+                println!("CalcReducer::reduce: + {}", i);
+                CalcState {
+                    count: state.count + i,
+                }
+            }
+            CalcAction::Subtract(i) => {
+                println!("CalcReducer::reduce: - {}", i);
+                CalcState {
+                    count: state.count - i,
+                }
+            }
+        }
+    }
+}
+
+struct CalcSubscriber {}
+
+impl Default for CalcSubscriber {
+    fn default() -> CalcSubscriber {
+        CalcSubscriber {}
+    }
+}
+
+impl Subscriber<CalcState, CalcAction> for CalcSubscriber {
+    fn notify(&self, state: &CalcState, action: &CalcAction) {
+        match action {
+            CalcAction::Add(i) => {
+                println!("CalcSubscriber::notify: state:{:?}, action:{}", state, i);
+            }
+            CalcAction::Subtract(i) => {
+                println!("CalcSubscriber::notify: state:{:?}, action: -{}", state, i);
+            }
+        }
+    }
+}
+
+pub fn main() {
+    println!("Hello, Calc!");
+
+    let mut store = Store::<CalcState, CalcAction>::default();
+    store.reducers.push(Box::new(CalcReducer::default()));
+    store.subscribers.push(Box::new(CalcSubscriber::default()));
+    let (tx, rx) = std::sync::mpsc::channel::<CalcAction>();
+    store.tx = Some(tx);
+    let dispatch_store = Arc::new(Mutex::new(store));
+    let tx_store = dispatch_store.clone();
+    let dispatcher = thread::spawn(move || {
+        for action in rx {
+            let new_state = dispatch_store.lock().unwrap().do_reduce(&action);
+            dispatch_store.lock().unwrap().do_notify(&new_state, &action);
+        }
+    });
+
+    tx_store.lock().unwrap().dispatch(CalcAction::Add(1));
+    tx_store.lock().unwrap().dispatch(CalcAction::Subtract(1));
+    tx_store.lock().unwrap().close();
+
+    dispatcher.join().unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,115 @@
-use std::sync::mpsc::{Receiver, Sender};
-use std::{sync, thread};
-use std::thread::JoinHandle;
+use std::sync::mpsc::Sender;
 
-
-pub struct Store<State, Action, Reducer>
-where
-    State: Default + Clone + Send + 'static,
-    Action: Clone + Send + 'static,
-    Reducer: Fn(State, Action) -> State,
-{
-    state: State,
-   // dispatcher: JoinHandle<()>,
-    rx: Receiver<Action>,
-    tx: Sender<Action>,
-    reducers: Vec<Reducer>,
-    //subscriber: Vec<Subscriber>,
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum StoreError {
+    #[error("no error")]
+    NoError,
+    #[error("send error {inner}")]
+    SendError {inner: String },
+    #[error("lock error {inner}")]
+    LockError {inner: String },
 }
 
-impl<State, Action, Reducer> Store<State, Action, Reducer>
+
+pub trait Reducer<State, Action>
+    where
+        State: Default + Send + Sync + Clone,
+        Action: Send + Sync,
 {
-    fn new(state: State, reducer: Reducer) -> State {
+    fn reduce(&self, state: &State, action: &Action) -> State;
 
-        // dispatcher thread
-        let (tx, rx) = sync::mpsc::channel::<Action>();
-        let dispatch_thread = thread::spawn(|| {
-           for action in rx {
-               println!("action: {}", action);
-           }
-        });
+    /// <https://doc.rust-lang.org/book/ch10-02-traits.html>
+    #[allow(clippy::all)]
+    fn new() -> Box<dyn Reducer<State, Action> + Send + Sync>
+        where
+            Self: Default + Sized + Sync + Send + 'static,
+    {
+        Box::new(Self::default())
+    }
+}
 
-        State {
-            state,
-            reducer,
-            rx,
-            tx,
+pub trait Subscriber<State, Action>
+    where
+        State: Default + Send + Sync + Clone,
+        Action: Send + Sync,
+{
+    fn notify(&self, state: &State, action: &Action);
+
+    /// <https://doc.rust-lang.org/book/ch10-02-traits.html>
+    #[allow(clippy::all)]
+    fn new() -> Box<dyn Subscriber<State, Action> + Send + Sync>
+        where
+            Self: Default + Sized + Sync + Send + 'static,
+    {
+        Box::new(Self::default())
+    }
+}
+
+pub struct Store<State, Action>
+    where
+        State: Default + Send + Sync + Clone,
+        Action: Send + Sync,
+{
+    pub state: State,
+    pub reducers: Vec<Box<dyn Reducer<State,Action> + Send + Sync>>,
+    pub subscribers: Vec<Box<dyn Subscriber<State,Action> + Send + Sync>>,
+    pub tx: Option<Sender<Action>>,
+}
+
+impl<State, Action> Default for Store<State,Action>
+    where
+        State: Default + Send + Sync + Clone,
+        Action: Send + Sync,
+{
+    fn default() -> Store<State,Action> {
+        Store {
+            state: Default::default(),
+            reducers: Vec::default(),
+            subscribers: Vec::default(),
+            tx: None,
+        }
+    }
+}
+
+impl<State, Action> Store<State, Action>
+    where
+        State: Default + Send + Sync + Clone,
+        Action: Send + Sync,
+{
+    pub fn add_reducer(&mut self, reducer: Box<dyn Reducer<State, Action> + Send + Sync>) {
+        self.reducers.push(reducer);
+    }
+
+    pub fn add_subscriber(&mut self, subscriber: Box<dyn Subscriber<State, Action> + Send + Sync>) {
+        self.subscribers.push(subscriber);
+    }
+
+    pub fn do_reduce(&mut self, action: &Action) -> State {
+        for reducer in &self.reducers {
+            self.state = reducer.reduce(&self.state, action);
+        }
+        self.state.clone()
+    }
+
+    pub fn do_notify(&mut self, state: &State, action: &Action) {
+        for subscriber in &self.subscribers {
+            subscriber.notify(state, action);
         }
     }
 
-    fn close(&self) {}
-    pub fn dispatch(&self, action: Action) {}
-    pub fn subscribe() {}
+    pub fn dispatch(&self, action: Action) {
+        if let Some(tx) = &self.tx {
+            tx.send(action).unwrap_or(());
+        }
+    }
 
-
+    pub fn close(&mut self) {
+        match self.tx.take() {
+            Some(tx) => drop(tx),
+            None => (),
+        }
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-}
+mod tests {}
