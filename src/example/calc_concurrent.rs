90ea66de579babc5b64fd8ff54fa64c5bce8b968
Changju <rookiecj@gmail.com>
2024-04-18 03:15:54 +0900

    add an example

---
+++ src/example/calc_concurrent.rs
@@ -0,0 +1,115 @@
+use std::thread;
+
+use rs_store::Dispatcher;
+use rs_store::{Reducer, Store, Subscriber};
+
+#[derive(Debug)]
+enum CalcAction {
+    Add(i32),
+    Subtract(i32),
+}
+
+struct CalcReducer {}
+
+impl Default for CalcReducer {
+    fn default() -> CalcReducer {
+        CalcReducer {}
+    }
+}
+
+#[derive(Debug, Clone)]
+struct CalcState {
+    count: i32,
+}
+
+impl Default for CalcState {
+    fn default() -> CalcState {
+        CalcState { count: 0 }
+    }
+}
+
+impl Reducer<CalcState, CalcAction> for CalcReducer {
+    fn reduce(&self, state: &CalcState, action: &CalcAction) -> CalcState {
+        match action {
+            CalcAction::Add(i) => {
+                println!("CalcReducer::reduce: + {}", i);
+                CalcState {
+                    count: state.count + i,
+                }
+            }
+            CalcAction::Subtract(i) => {
+                println!("CalcReducer::reduce: - {}", i);
+                CalcState {
+                    count: state.count - i,
+                }
+            }
+        }
+    }
+}
+
+struct CalcSubscriber {
+    id: i32,
+    last: CalcState,
+}
+
+impl Default for CalcSubscriber {
+    fn default() -> CalcSubscriber {
+        CalcSubscriber {
+            id: 0,
+            last: CalcState::default(),
+        }
+    }
+}
+
+impl CalcSubscriber {
+    fn new(id: i32) -> CalcSubscriber {
+        CalcSubscriber {
+            id,
+            last: CalcState::default(),
+        }
+    }
+}
+
+impl Subscriber<CalcState, CalcAction> for CalcSubscriber {
+    fn notify(&mut self, state: &CalcState, action: &CalcAction) {
+        println!("CalcSubscriber::notify: {}, action: {:?}", self.id, action);
+
+        match action {
+            CalcAction::Add(i) => {
+                println!(
+                    "CalcSubscriber::notify: {}, state: {:?} <- old: {:?}",
+                    self.id, state, self.last
+                );
+            }
+            CalcAction::Subtract(i) => {
+                println!(
+                    "CalcSubscriber::notify: {}, state: {:?} <- old: {:?}",
+                    self.id, state, self.last
+                );
+            }
+        }
+
+        self.last = state.clone();
+    }
+}
+
+pub fn main() {
+    println!("Hello, Calc!");
+
+    let store = Store::<CalcState, CalcAction>::new(Box::new(CalcReducer::default()));
+
+    store.add_subscriber(Box::new(CalcSubscriber::default()));
+    store.dispatch(CalcAction::Add(1));
+
+    let store_clone = store.clone();
+    thread::spawn(move || {
+        thread::sleep(std::time::Duration::from_secs(1));
+
+        store_clone.add_subscriber(Box::new(CalcSubscriber::new(1)));
+        store_clone.dispatch(CalcAction::Subtract(1));
+    })
+    .join()
+    .unwrap();
+
+    store.stop();
+}
